d4b07cebea5058b22882fb98cfd2f953938b24b7
azurecurve <development@azurecurve.co.uk>
2019-03-21 18:54:22 +0000

    v1.0.0

---
+++ azrcrv-icons.php
@@ -0,0 +1,228 @@
+<?php
+/**
+ * ------------------------------------------------------------------------------
+ * Plugin Name: Icons
+ * Description: Allows icons to be added to posts and pages using a shortcode.
+ * Version: 1.0.0
+ * Author: azurecurve
+ * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/icons
+ * Text Domain: icons
+ * Domain Path: /languages
+ * ------------------------------------------------------------------------------
+ * This is free software released under the terms of the General Public License,
+ * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Full
+ * text of the license is available at https://www.gnu.org/licenses/gpl-2.0.html.
+ * ------------------------------------------------------------------------------
+ */
+
+// include plugin menu
+require_once(dirname( __FILE__).'/pluginmenu/menu.php');
+
+// Prevent direct access.
+if (!defined('ABSPATH')){
+	die();
+}
+
+/**
+ * Setup actions, filters and shortcodes.
+ *
+ * @since 1.0.0
+ *
+ */
+// add actions
+add_action('admin_menu', 'azrcrv_i_create_admin_menu');
+add_action('wp_enqueue_scripts', 'azrcrv_i_load_css');
+//add_action('the_posts', 'azrcrv_i_check_for_shortcode');
+
+// add filters
+add_filter('plugin_action_links', 'azrcrv_i_add_plugin_action_link', 10, 2);
+
+// add shortcodes
+add_shortcode('icon', 'azrcrv_i_icon');
+add_shortcode('ICON', 'azrcrv_i_icon');
+
+/**
+ * Check if shortcode on current page and then load css and jqeury.
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_i_check_for_shortcode($posts){
+    if (empty($posts)){
+        return $posts;
+	}
+	
+	
+	// array of shortcodes to search for
+	$shortcodes = array(
+						'icon','ICON'
+						);
+	
+    // loop through posts
+    $found = false;
+    foreach ($posts as $post){
+		// loop through shortcodes
+		foreach ($shortcodes as $shortcode){
+			// check the post content for the shortcode
+			if (has_shortcode($post->post_content, $shortcode)){
+				$found = true;
+				// break loop as shortcode found in page content
+				break 2;
+			}
+		}
+	}
+ 
+    if ($found){
+		// as shortcode found call functions to load css and jquery
+        azrcrv_i_load_css();
+    }
+    return $posts;
+}
+	
+/**
+ * Load plugin css.
+ *
+ * @since 1.0.0
+ *
+	 */
+function azrcrv_i_load_css(){
+	wp_enqueue_style('azrcrv-i', plugins_url('assets/css/style.css', __FILE__));
+}
+
+/**
+ * Add Icons action link on plugins page.
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_i_add_plugin_action_link($links, $file){
+	static $this_plugin;
+
+	if (!$this_plugin){
+		$this_plugin = plugin_basename(__FILE__);
+	}
+
+	if ($file == $this_plugin){
+		$settings_link = '<a href="'.get_bloginfo('wpurl').'/wp-admin/admin.php?page=azrcrv-i">'.esc_html__('Settings' ,'icons').'</a>';
+		array_unshift($links, $settings_link);
+	}
+
+	return $links;
+}
+
+/**
+ * Add Icon menu to plugin menu.
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_i_create_admin_menu(){
+	//global $admin_page_hooks;
+	
+	add_submenu_page("azrcrv-plugin-menu"
+						,esc_html__("Icons Settings", "icons")
+						,esc_html__("Icons", "icons")
+						,'manage_options'
+						,'azrcrv-i'
+						,'azrcrv_i_settings');
+}
+
+/**
+ * Display Settings page.
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_i_settings(){
+	if (!current_user_can('manage_options')){
+		$error = new WP_Error('not_found', esc_html__('You do not have sufficient permissions to access this page.' , 'icons'), array('response' => '200'));
+		if(is_wp_error($error)){
+			wp_die($error, '', $error->get_error_data());
+		}
+	}
+	?>
+	<div id="azrcrv-i-general" class="wrap">
+		<h2><?php echo esc_html(get_admin_page_title()); ?></h2>
+
+		<label for="explanation">
+				<p><?php esc_html_e('Icons allows a 16x16 icon to be displayed in a post or page using the [icon] shortcode.', 'icons'); ?></p>
+				<p><?php esc_html_e('Format of shortcode is [icon=accept] to display the accept icon.', 'icons'); ?></p>
+				<p><?php esc_html_e("Included icons are from the famfamfam Silk icon set 1.3 by Mark James (<a href='http://www.famfamfam.com/lab/icons/silk/'>http://www.famfamfam.com/lab/icons/silk/</a>). Extra icons can be added by simply placing them in PNG format into the /images folder; the filename, without the extension, is the shortcode parameter.", "icons"); ?></p>
+			</label>
+		</label>
+		
+		<p>
+		<?php esc_html_e('Available icons are:', 'icons');
+			
+			$dir = plugin_dir_path(__FILE__).'/images';
+			if (is_dir($dir)){
+				if ($directory = opendir($dir)){
+					while (($file = readdir($directory)) !== false){
+						if ($file != '.' and $file != '..' and $file != 'Thumbs.db'){
+							$filewithoutext = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file);
+							echo "<div style='width: 180px; display: inline-block;'><img src='";
+							echo plugin_dir_url(__FILE__)."images/".esc_html($filewithoutext).".png;' alt='".esc_html($filewithoutext)."' />&nbsp;<em>".esc_html($filewithoutext)."</em></div>";
+						}
+					}
+					closedir($directory);
+				}
+			}
+			?>
+		</p>
+		
+		<label for="additional-plugins">
+			azurecurve <?php esc_html_e('has the following plugins which allow shortcodes to be used in comments and widgets:', 'icons'); ?>
+		</label>
+		<ul class='azrcrv-plugin-index'>
+			<li>
+				<?php
+				if (azrcrv_i_is_plugin_active('azrcrv-shortcodes-in-comments/azrcrv-shortcodes-in-comments.php')){
+					echo "<a href='admin.php?page=azrcrv-sic' class='azrcrv-plugin-index'>Shortcodes in Comments</a>";
+				}else{
+					echo "<a href='https://plugins.classicpress.org/shortcodes-in-comments/' class='azrcrv-plugin-index'>Shortcodes in Comments</a>";
+				}
+				?>
+			</li>
+			<li>
+				<?php
+				if (azrcrv_i_is_plugin_active('azrcrv-shortcodes-in-widgets/azrcrv-shortcodes-in-widgets.php')){
+					echo "<a href='admin.php?page=azrcrv-siw' class='azrcrv-plugin-index'>Shortcodes in Widgets</a>";
+				}else{
+					echo "<a href='https://plugins.classicpress.org/shortcodes-in-widgets/' class='azrcrv-plugin-index'>Shortcodes in Widgets</a>";
+				}
+				?>
+			</li>
+		</ul>
+	</div><?php
+}
+
+/**
+ * Check if function active (included due to standard function failing due to order of load).
+ *
+ * @since 1.0.0
+ *
+ */
+function azrcrv_i_is_plugin_active($plugin){
+    return in_array($plugin, (array) get_option('active_plugins', array()));
+}
+
+/**
+ * Icon shortcode.
+ *
+ * @since 1.0.0
+ *
+	 */
+function azrcrv_i_icon($atts, $content = null)
+{
+	if (empty($atts)){
+		$icon = 'none';
+	}else{
+		$attribs = implode('',$atts);
+		$icon = trim (trim (trim (trim (trim ($attribs , '=') , '"') , "'") , '&#8217;') , "&#8221;");
+	}
+	return "<img class='azrcrv-i' src='".plugin_dir_url(__FILE__)."images/".esc_html($icon).".png' alt= '".esc_html($icon)."' />";
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Icons ===
+Contributors: azurecurve
+Tags: icon, icons, posts, pages
+Donate link: https://development.azurecurve.co.uk/support-development/
+Requires at least: 1.0.0
+Tested up to: 1.0.0
+Requires PHP: 5.6
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Allows a 16x16 icon to be displayed in a post or page using a shortcode.
+
+== Description ==
+Easily add an icon to a post or page using the [icon] shortcode.
+
+For example, to display the note icon, shortcode usage is [icon=note]; 1,000 icons from the famfamfam Silk collection are included.
+
+Defintion of icons can be found at famfamfam.com: http://www.famfamfam.com/lab/icons/silk/; a settings page also shows all available icons.
+
+Adding any png file to the plugins /images folder will make them available from the shortcode; this allows the replacement of the included flags with alternatives if required.
+
+<a href='https://development.azurecurve.co.uk/classicpress-plugins/shortcode-in-comments/'>Shortcodes In Comments</a> can be used to allow flags in comments and <a href='https://development.azurecurve.co.uk/classicpress-plugins/shortcode-in-widgets/'>Shortcodes In Widgets</a> can allow them in widgets.
+
+== Installation ==
+To install the Icons plugin:
+* Download the plugin from <a href='https://github.com/azurecurve/azrcrv-icons/'>GitHub</a>.
+* Upload the entire zip file using the Plugins upload function in your ClassicPress admin panel.
+* Activate the plugin.
+
+== Changelog ==
+Changes and feature additions for the Icons plugin:
+= 1.0.0 =
+* First version for ClassicPress forked from azurecurve Icons WordPress Plugin.
+
+== Frequently Asked Questions ==
+= Can I translate this plugin? =
+* Yes, the .pot fie is in the plugin's languages folder and can also be downloaded from the plugin page on https://development.azurecurve.co.uk; if you do translate this plugin please sent the .po and .mo files to translations@azurecurve.co.uk for inclusion in the next version (full credit will be given).
+= Is this plugin compatible with both WordPress and ClassicPress? =
+* This plugin is developed for ClassicPress, but will likely work on WordPress.
